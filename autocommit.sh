640d758fe40f75f1283453a98e7facd4c6ae993d
EricZimmerman <saericzimmerman@gmail.com>
2022-12-01 14:38:13 -0500

    Autocommit from 2022-12-01 14:38:13
    Klipper on commit: 5b1a667
    
    Moonraker on commit: f3f39b7
    
    Mainsail version: v2.4.0

--- autocommit.sh
+++ autocommit.sh
@@ -57,7 +57,7 @@ grab_version(){
 
 push_config(){
   cd $config_folder
-  git pull
+  git pull origin master
   git add .
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
